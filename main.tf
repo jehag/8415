ab0d02b6f988ec1a26a637a03af98e99ae8d9d95
jehag <jeremy.hage@polymtl.ca>
2022-12-23 09:15:35 -0500

    added comments for lisibility

--- main.tf
+++ main.tf
@@ -9,10 +9,12 @@ terraform {
     required_version = ">= 1.2.0"
 }
 
+# Configure the AWS provider.
 provider "aws" {
     region = "us-east-1"
 }
 
+# Create a security group accessible from outside the vpc.
 resource "aws_security_group" "public" {
     name = "public"
     vpc_id = "vpc-12345678"
@@ -33,10 +35,12 @@ resource "aws_security_group" "public" {
     }
 }
 
+# Create a security group for the cluster.
 resource "aws_security_group" "private" {
     name = "private"
     vpc_id = "vpc-0618bc87676a9671b"
 
+    # SSH port.
     ingress {
         protocol = "-1"
         from_port = 22
@@ -44,6 +48,7 @@ resource "aws_security_group" "private" {
         cidr_blocks = ["172.31.0.0/16"]
     }
 
+    # Cluster management node port.
     ingress {
         protocol = "-1"
         from_port = 1186
@@ -51,6 +56,7 @@ resource "aws_security_group" "private" {
         cidr_blocks = ["172.31.0.0/16"]
     }
 
+    # Cluster data nodes port.
     ingress {
         protocol = "-1"
         from_port = 2202
@@ -58,6 +64,7 @@ resource "aws_security_group" "private" {
         cidr_blocks = ["172.31.0.0/16"]
     }
 
+    # MySQL server port.
     ingress {
         protocol = "-1"
         from_port = 3306
@@ -73,6 +80,7 @@ resource "aws_security_group" "private" {
     }
 }
 
+# Create the cluster master.
 resource "aws_instance" "master" {
     ami = "ami-0a6b2839d44d781b2"
     instance_type = "t2.micro"
@@ -83,6 +91,7 @@ resource "aws_instance" "master" {
     vpc_security_group_ids = [aws_security_group.private.id]
 }
 
+# Create the cluster subordinates.
 resource "aws_instance" "subordinates" {
     count = 3
     ami = "ami-0a6b2839d44d781b2"
@@ -94,6 +103,7 @@ resource "aws_instance" "subordinates" {
     vpc_security_group_ids = [aws_security_group.private.id]
 }
 
+# Create the proxy.
 resource "aws_instance" "proxy" {
     ami = "ami-0a6b2839d44d781b2"
     instance_type = "t2.micro"
@@ -104,6 +114,7 @@ resource "aws_instance" "proxy" {
     vpc_security_group_ids = [aws_security_group.public.id]
 }
 
+# Create the stand-alone.
 resource "aws_instance" "stand-alone" {
     ami = "ami-0a6b2839d44d781b2"
     instance_type = "t2.micro"
